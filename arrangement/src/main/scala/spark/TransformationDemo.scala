f8c54e28e4299c8bd2d8db8f0b6be70b4f68a42c
YuiSol <914694648@qq.com>
2017-09-13 19:39:39 +0800

    1

--- arrangement/src/main/scala/spark/TransformationDemo.scala
+++ arrangement/src/main/scala/spark/TransformationDemo.scala
@@ -8,11 +8,12 @@ import org.apache.spark.{SparkConf, SparkContext}
  */
 object TransformationDemo {
   def main(args: Array[String]) {
-    val master: SparkConf = new SparkConf().setAppName(this.getClass.getName).setMaster("local")
+    val master: SparkConf = new SparkConf().setAppName(this.getClass.getName).setMaster("local[5]")
     val sc: SparkContext = new SparkContext(master)
     /*filterdemo(sc)*/
     /*flatMapDemo(sc)*/
-     mapPartitionDemo(sc)
+   /*  mapPartitionDemo(sc)*/
+     mapPartitionDemo1(sc)
   /*  println(rdd.collect().toBuffer)*/
   }
   def filterdemo(sc :SparkContext): Unit ={
@@ -33,11 +34,27 @@ object TransformationDemo {
   }
   def mapPartitionDemo(sc : SparkContext)= {
     val rdd: RDD[Int] = sc.parallelize(Array(1,2,3,4,6,7,93,2,32,54,3,546,3,345,1,3))
+    /**
+     * index 分区数
+     * iter 对应的分区的内容
+     * 一个rdd下一系列的分区 弹性式分布式数据集
+     * 要传入一个函数
+     * 返回一个 scala.collection.Iterator[A]
+     * 一个RDD  =》 多个 分区 = 》内容
+     * 每一个函数作用到每个函数上  iter内的每一个分区都会作用到该函数
+     *
+     */
     val rdd1: RDD[Unit] = rdd.mapPartitionsWithIndex((index: Int, iter: Iterator[Int]) => {
       iter.toList.map(x => {
-        println(x)
+        println(s"index = $index"+s"partition content = $x")
+        /*println(x)*/
       }).iterator
     })
     rdd1.collect()
+  }
+  def mapPartitionDemo1(sc : SparkContext)= {
+    val rdd: RDD[Int] = sc.parallelize(Array(1,2,3,4,6,7,93,2,32,54,3,546,3,345,1,3))
+    val rdd1: RDD[Int] = rdd.mapPartitions(x =>x.map(x=>x*100))
+    println(rdd1.collect().toBuffer)
   }
 }
